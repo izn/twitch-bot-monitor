244cd673d6645837870cf238982d406c089daed8
T. Bruno Moretti <moretty@gmail.com>
2017-11-17 14:30:09 -0200

    Rebuild project structure and install dotenv

---
+++ bot/bot.py
@@ -0,0 +1,50 @@
+import dotenv
+import socket
+import string
+import os
+import re
+import redis
+import unicodedata
+
+
+dotenv.load_dotenv(dotenv.find_dotenv())
+
+r = redis.Redis()
+
+server = 'irc.chat.twitch.tv'
+nick = os.environ.get('TWITCH_LOGIN')
+passwd = os.environ.get('TWITCH_OAUTH')
+channel = os.environ.get('TWITCH_CHANNEL')
+port = 6667
+
+s = socket.socket()
+s.connect((server, port))
+s.send(bytes('PASS %s\r\n' % passwd, 'utf-8'))
+s.send(bytes('NICK %s\r\n' % nick, 'utf-8'))
+s.send(bytes('JOIN #%s\r\n' % channel.lower(), 'utf-8'))
+
+while 1:
+    buffered = s.recv(1024)
+    data = buffered.decode('utf-8')
+
+    # irc-protocol expressions
+    ping = re.search(r'^PING', data)
+    privmsg = re.search(r'^:(\w+)!(?:\w+)@(?:[\w\.]+)\sPRIVMSG\s#(\w+)\s:(.+?)\r\n$', data)
+
+    if ping is not None:
+        print("[PING] Sending PONG :)")
+        s.send(b'PONG :tmi.twitch.tv\r\n')
+
+    if privmsg is not None:
+        _user = privmsg.group(1).lower()
+        _channel = privmsg.group(2).lower()
+        _msg = privmsg.group(3).lower()
+
+        print('#{0} @ {1}: {2}'.format(_channel, _user, _msg))
+
+        for msg in list(set(_msg.split(" "))):
+            filtered_word = msg.strip(string.punctuation)
+            filtered_word = unicodedata.normalize('NFD', filtered_word)
+
+            if len(filtered_word) > 2:
+                r.zincrby(_channel, filtered_word, 1)
---
+++ bot/server.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import dotenv
+import flask
+import os
+import redis
+
+
+dotenv.load_dotenv(dotenv.find_dotenv())
+
+app = flask.Flask(__name__)
+r = redis.Redis(decode_responses=True)
+
+channel = os.environ.get('TWITCH_CHANNEL')
+
+
+def parse_messages():
+    stats = r.zrevrange(channel, 0, 21, withscores=True)
+    return [{'term': w, 'ocurrencies': o} for w, o in stats]
+
+
+@app.context_processor
+def inject_vars():
+    return {'channel': channel}
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return flask.render_template('index.html')
+
+
+@app.route('/stream', methods=['GET'])
+def stream():
+    return flask.jsonify(parse_messages())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
